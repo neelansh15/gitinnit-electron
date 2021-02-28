6a5f2a8f9006c908b667e6c54e2e4b1076d13bee
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-02-28 22:55:30 +0530

    Move sidebar to global layout

--- src/renderer/layouts/default.vue
+++ src/renderer/layouts/default.vue
@@ -1,5 +1,40 @@
 <template>
   <v-app>
+     <!--Left div-->
+    <div class="row">
+      <div class="col s6">
+        <div class="userInfo">
+          <h3>img</h3>
+          <h3>Username</h3>
+        </div>
+        <div class="generalLinks">
+          <!--Vertical Nav-->
+          <h3>General</h3>
+          <ul>
+            <li>Home</li>
+            <li>Projects</li>
+            <li>
+              <button>
+                <nuxt-link to="/startProject" exact>
+                  Start a project
+                </nuxt-link>
+              </button>
+            </li>
+            <li>Manage Samples</li>
+          </ul>
+        </div>
+        <div class="accountLinks">
+          <h3>
+            <nuxt-link to="/account">
+              Account
+            </nuxt-link>
+          </h3>
+          <ul>
+            <li>Sign out</li>
+          </ul>
+        </div>
+      </div>
+    </div>
     <nuxt />
   </v-app>
 </template>
--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -1,39 +1,6 @@
 <template>
   <div>
-    <!--Left div-->
-    <div class="row">
-      <div class="col s6">
-        <div class="userInfo">
-          <h3>img</h3>
-          <h3>Username</h3>
-        </div>
-        <div class="generalLinks">
-          <!--Vertical Nav-->
-          <h3>General</h3>
-          <ul>
-            <li>Home</li>
-            <li>Projects</li>
-            <li>
-              <button>
-                <nuxt-link to="/startProject" exact>
-                  Start a project
-                </nuxt-link>
-              </button>
-            </li>
-            <li>Manage Samples</li>
-          </ul>
-        </div>
-        <div class="accountLinks">
-          <h3>
-            <nuxt-link to="/account">
-              Account
-            </nuxt-link>
-          </h3>
-          <ul>
-            <li>Sign out</li>
-          </ul>
-        </div>
-      </div>
+   
 
       <!--Right Div-->
       <div class="col s6">
