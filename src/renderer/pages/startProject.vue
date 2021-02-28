ad3941dde24df10c605fc7cbf040b624bbb68a81
therookie636 <therookie636@gmail.com>
2021-02-28 22:41:00 +0530

    Start Project basic structure completed

--- src/renderer/pages/startProject.vue
+++ src/renderer/pages/startProject.vue
@@ -1,82 +1,140 @@
 <template>
   <div id="container">
-    <v-stepper v-model="e1">
-      <v-stepper-header>
-        <v-stepper-step :complete="e1 > 1" step="1">
-          Select folder
-        </v-stepper-step>
+    <h1>Start a new project</h1>
+    <v-app>
+      <v-stepper v-model="e1">
+        <v-stepper-header>
+          <v-stepper-step :complete="e1 > 1" step="1">
+            Select folder
+          </v-stepper-step>
 
-        <v-divider />
+          <v-divider />
 
-        <v-stepper-step :complete="e1 > 2" step="2">
-          Add description
-        </v-stepper-step>
+          <v-stepper-step :complete="e1 > 2" step="2">
+            Add description
+          </v-stepper-step>
 
-        <v-divider />
+          <v-divider />
 
-        <v-stepper-step step="3">
-          Confirm Project details
-        </v-stepper-step>
-      </v-stepper-header>
+          <v-stepper-step step="3">
+            Confirm Project details
+          </v-stepper-step>
+        </v-stepper-header>
 
-      <v-stepper-items>
-        <v-stepper-content step="1">
-          <div class="input">
-            <v-file-input
-              label="Select file to get path"
-              @change="handleFileChange"
+        <v-stepper-items>
+          <v-stepper-content step="1">
+            <div class="input">
+              <v-file-input
+                label="Select file to get path"
+                @change="handleFileChange"
+              >
+                Select Folder
+              </v-file-input>
+            </div>
+            <div class="path">
+              <h5>Folder:</h5>
+              <p>{{ folder }}</p>
+            </div>
+            <p>{{ message }}</p>
+
+            <v-btn :disabled="invalid" color="primary" @click="e1 = 2">
+              Continue
+            </v-btn>
+
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
+
+          <v-stepper-content step="2">
+            <v-form
+              ref="form"
+              v-model="valid"
             >
-              Select Folder
-            </v-file-input>
-          </div>
-          <div class="path">
-            <h5>Folder:</h5>
-            <p>{{ folder }}</p>
-          </div>
-          <button id="select" @click="pathConfirmed">Confirm</button>
-          <p>{{ message }}</p>
+              <v-text-field
+                v-model="name"
+                :counter="10"
+                :rules="nameRules"
+                label="Project name"
+                required
+              />
 
-          <v-btn color="primary" @click="e1 = 2">
-            Continue
-          </v-btn>
+              <v-text-field
+                v-model="author"
+                :rules="nameRules"
+                label="Author"
+                required
+              />
+              <v-text-field
+                v-model="description"
+                :rules="descriptionRules"
+                label="Description"
+                required
+              />
 
-          <v-btn text>
-            Cancel
-          </v-btn>
-        </v-stepper-content>
+              <v-text-field
+                v-model="genre"
+                label="Genre"
+              />
+              <v-select
+                v-model="daw"
+                :items="dawItems"
+                label="daw"
+                required
+              />
 
-        <v-stepper-content step="2">
-          <v-card class="mb-12" color="grey lighten-1" height="200px" />
+              <p>Allow this app to handle my git requests and make changes in my folder</p>
+              <v-checkbox
+                v-model="checkbox"
+                :rules="[v => !!v || 'You must agree to continue!']"
+                label="Do you agree?"
+                required
+              />
 
-          <v-btn color="primary" @click="e1 = 3">
-            Continue
-          </v-btn>
+              <v-btn
+                :disabled="!valid"
+                color="primary"
+                class="mr-4"
+                @click="e1 = 3"
+              >
+                Validate
+              </v-btn>
+            </v-form>
 
-          <v-btn text>
-            Cancel
-          </v-btn>
-        </v-stepper-content>
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
 
-        <v-stepper-content step="3">
-          <v-card class="mb-12" color="grey lighten-1" height="200px" />
+          <v-stepper-content step="3">
+            <div>
+              <h1>Project: {{ name }}</h1>
+              <h2>Author: {{ author }}</h2>
+              <p>Description: {{ description }}</p>
+              <p>Genre: {{ genre }}</p>
+              <p>daw: {{ daw }}</p>
+              <p>Folder: {{ folder }}</p>
+            </div>
 
-          <v-btn color="primary" @click="e1 = 1">
-            Continue
-          </v-btn>
+            <v-btn color="primary" @click="e1 = 1">
+              Continue
+            </v-btn>
 
-          <v-btn text>
-            Cancel
-          </v-btn>
-        </v-stepper-content>
-      </v-stepper-items>
-    </v-stepper>
-    <h1>This is push Module</h1>
-
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
+        </v-stepper-items>
+      </v-stepper>
+    </v-app>
     <div>
       <nuxt-link to="/">
         Back
       </nuxt-link>
     </div>
+    <nuxt-link to="/" exact>
+      Back
+    </nuxt-link>
   </div>
 </template>
 
@@ -87,7 +145,25 @@ export default {
       path: 'none',
       folder: 'none',
       message: '',
-      e1: 1
+      e1: 1,
+      valid: false,
+      invalid: true,
+      dawItems: ['FL Studio', 'Ableton Live', 'Reason', 'Logic Pro X', 'Cubase'],
+      name: '',
+      author: '',
+      description: '',
+      genre: '',
+      daw: '',
+      checkbox: false,
+
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length < 10) || 'Name must be less than 10 characters'
+      ],
+      descriptionRules: [
+        v => !!v || 'Description is required',
+        v => (v && v.length < 50) || 'Description must be less than 50 characters'
+      ]
     }
   },
   methods: {
@@ -95,6 +171,7 @@ export default {
       if (file == null) {
         this.path = 'none'
         this.folder = 'No folder choosen'
+        this.invalid = true
         return
       }
       let pathToFile = file.path
@@ -102,14 +179,7 @@ export default {
       pathToFile = pathToFile.slice(0, index)
       this.path = pathToFile
       this.folder = pathToFile.slice(pathToFile.lastIndexOf('\\') + 1)
-    },
-    pathConfirmed () {
-      if (this.path === 'none') {
-        this.message = 'No folder choosen'
-        return
-      }
-      this.message = 'git init'
-      console.log(this.path)
+      this.invalid = false
     }
   }
 }
@@ -117,6 +187,7 @@ export default {
 
 <style>
 #container {
+  margin-top: 100px;
   width: 100vw;
   height: 90vh;
   display: flex;
