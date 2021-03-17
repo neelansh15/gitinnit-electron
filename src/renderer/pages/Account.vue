a737e1eee33ce5d9400a53dfaa5b92f8df8ab2f8
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-03-17 21:55:36 +0530

    Account: Add avatar and name design

--- src/renderer/pages/Account.vue
+++ src/renderer/pages/Account.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <h1>Account</h1>
+  <div class="mx-5 mt-5">
+    <div class="d-flex align-center">
+      <v-img
+       src="https://img.icons8.com/bubbles/100/000000/gender-neutral-user.png"
+       width="100"
+       height="100"
+       class="flex-grow-0"
+       ></v-img>
+      <div class="mt-4">
+        <h3>Neelansh Mathur</h3>
+        <p class="text-caption">@mneelansh15</p>
+      </div>
+    </div>
     <v-btn @click="newWindow">New Window (Testing)</v-btn>
     <v-btn @click="login">Login</v-btn>
   </div>
