f3294706cb94f440e80347b278a33e16c25fba19
pss10 <parthsatyen@gmail.com>
2021-04-09 07:31:32 +0400

    This is drop down menu

--- src/renderer/components/Combo.vue
+++ src/renderer/components/Combo.vue
@@ -1,29 +1,44 @@
-
 <template>
-   
-        <v-combobox
-          v-model="select"
-          :items="items"
-          label="Combobox"
-          multiple
-          outlined
+  <v-container fluid>
+    <v-row align="center">
+      <v-col
+        class="d-flex"
+        cols="12"
+        sm="6"
+      >
+        <v-select
+          :items="repos"
+          label="Select Repository"
           dense
-        >
-        </v-combobox>
-    
+          outlined
+        ></v-select>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
 <script>
   export default {
     data () {
       return {
-        select: ['Vuetify', 'Programming'],
-        items: [
-          'Programming',
-          'Design',
-          'Vue',
-          'Vuetify',
-        ],
-      }
+            access_token: "",
+            repos: [],
+        }
     },
+    methods: {
+    async fetchRepos() {
+      axios
+        .get(`https://api.github.com/user/repos`, {
+          headers: {
+            Accept: "application/vnd.github.v3+json",
+            Authorization: "token " + this.access_token
+          }
+        })
+        .then(res => {
+          this.repos = res.data;
+          console.log(res.data);
+        });
+    },
+  }
   }
 </script>
