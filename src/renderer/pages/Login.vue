82897d477a591cfb3cd9866b1c777792aee4e15c
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-04-13 02:44:18 +0530

    Login: Modify design

--- src/renderer/pages/Login.vue
+++ src/renderer/pages/Login.vue
@@ -1,150 +1,148 @@
 <template>
-  <div class="mx-5 mt-5">
-    <div>
-      <v-card class="pa-5">
-        <h1 class="mb-3 text-h5">Request code and enter it in the Auth page</h1>
-        <v-btn depressed @click="login">Request code</v-btn>
+  <div>
+    <v-row justify="center">
+      <v-col md="6">
+        <v-card class="pa-5 mt-10">
+          <h1 class="mb-3 text-h5">
+            Request code and enter it in the Auth page
+          </h1>
+          <v-btn depressed @click="login">Request code</v-btn>
 
-        <v-icon>
-          mdi-arrow-right
-        </v-icon>
+          <v-icon> mdi-arrow-right </v-icon>
 
-        <v-btn
-          class="red lighten-1"
-          :disabled="
-            this.auth.verification_uri == '' ||
+          <v-btn
+            class="red lighten-1"
+            :disabled="
+              this.auth.verification_uri == '' ||
               this.auth.verification_uri == null
-          "
-          depressed
-          @click="openURL"
+            "
+            depressed
+            @click="openURL"
+          >
+            Go to Auth page
+          </v-btn>
+
+          <v-icon> mdi-arrow-right </v-icon>
+
+          <v-btn
+            :disabled="!auth.user_code || !clicked"
+            class="light-blue darken-1"
+            depressed
+            @click="verifyLogin"
+          >
+            Confirm and Login
+          </v-btn>
+        </v-card>
+
+        <v-alert
+          v-if="error != '' && error != null"
+          v-model="showError"
+          class="mt-5"
+          color="red lighten-2"
+          dismissible
         >
-          Go to Auth page
-        </v-btn>
+          {{ error }}
+        </v-alert>
 
-        <v-icon>
-          mdi-arrow-right
-        </v-icon>
-
-        <v-btn :disabled="!auth.user_code || !clicked" class="light-blue darken-1" depressed @click="verifyLogin">
-          Go forward
-        </v-btn>
-
-      </v-card>
-
-      <v-alert
-        v-if="error != '' && error != null"
-        v-model="showError"
-        class="mt-5"
-        color="red lighten-2"
-        dismissible
-      >
-        {{ error }}
-      </v-alert>
-
-      <h1 v-if="auth.user_code" class="text-h1 text-center mt-5">
-        {{ auth.user_code }}
-      </h1>
-
-      <!-- <v-footer v-if="auth.user_code" class="blue lighten-1" app>
-        Once you have entered the code in the browser's auth page,
-        <v-btn class="blue lighten-1" depressed @click="verifyLogin">
-          Go forward
-        </v-btn>
-        {{ final_token_obj.access_token }}
-      </v-footer> -->
-    </div>
+      </v-col>
+      <v-col cols="12">
+        <h1 v-if="auth.user_code" class="text-h1 text-center mt-5">
+          {{ auth.user_code }}
+        </h1>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-import { shell } from 'electron'
-import axios from 'axios'
-import { config } from '../oauth-config'
-import pkg from '../../../package.json'
+import { shell } from "electron";
+import axios from "axios";
+import { config } from "../oauth-config";
+import pkg from "../../../package.json";
 
-const { remote } = require('electron')
-const { app } = remote
-const qs = require('querystring')
-const fs = require('fs')
+const { remote } = require("electron");
+const { app } = remote;
+const qs = require("querystring");
+const fs = require("fs");
 
 export default {
   data: () => ({
     clicked: false,
     showError: false,
-    error: '',
+    error: "",
     auth: {},
-    final_token_obj: {} // Store in Vuex store though
+    final_token_obj: {}, // Store in Vuex store though
   }),
   methods: {
-    login () {
+    login() {
       // Request verification_url, device_code and user_code
-      const scope = 'repo%20read:user'
+      const scope = "repo%20read:user";
       axios
         .post(
           `https://github.com/login/device/code?client_id=${config.clientId}&scope=${scope}`
         )
-        .then(res => {
-          this.auth = qs.parse(res.data)
-          console.log(this.auth)
-        })
+        .then((res) => {
+          this.auth = qs.parse(res.data);
+          console.log(this.auth);
+        });
     },
-    openURL () {
-      shell.openExternal(this.auth.verification_uri)
-      this.clicked = true
+    openURL() {
+      shell.openExternal(this.auth.verification_uri);
+      this.clicked = true;
     },
-    verifyLogin () {
+    verifyLogin() {
       axios
         .post(
           `https://github.com/login/oauth/access_token?client_id=${config.clientId}&device_code=${this.auth.device_code}&grant_type=urn:ietf:params:oauth:grant-type:device_code`
         )
-        .then(res => {
-          const authResponse = qs.parse(res.data)
+        .then((res) => {
+          const authResponse = qs.parse(res.data);
           if (authResponse?.error) {
-            this.error = authResponse.error_description
-            this.showError = true
+            this.error = authResponse.error_description;
+            this.showError = true;
           } else {
-            this.final_token_obj = authResponse
-            console.log(authResponse)
+            this.final_token_obj = authResponse;
+            console.log(authResponse);
 
             // Set access token in the state
-            this.$store.commit('setAccessToken', authResponse.access_token)
+            this.$store.commit("setAccessToken", authResponse.access_token);
 
             // Set user data in a global config
-            const appDataPath = app.getPath('appData') + '\\' + pkg.name
-            const appDataGlobalConfigPath = appDataPath + '\\globalConfig.json'
-            console.log(appDataGlobalConfigPath)
+            const appDataPath = app.getPath("appData") + "\\" + pkg.name;
+            const appDataGlobalConfigPath = appDataPath + "\\globalConfig.json";
+            console.log(appDataGlobalConfigPath);
 
             if (!fs.existsSync(appDataPath)) {
-              fs.mkdirSync(appDataPath)
+              fs.mkdirSync(appDataPath);
             }
 
-            let globalConfigData
+            let globalConfigData;
             if (!fs.existsSync(appDataGlobalConfigPath)) {
-              fs.openSync(appDataGlobalConfigPath, 'w+')
-              globalConfigData = {}
+              fs.openSync(appDataGlobalConfigPath, "w+");
+              globalConfigData = {};
             } else {
               // Previous globalConfig exists
               globalConfigData = JSON.parse(
                 fs.readFileSync(appDataGlobalConfigPath)
-              )
+              );
             }
             // If there is already a globalConfig.json from a previous login, just update that
-            globalConfigData.access_token = authResponse.access_token
+            globalConfigData.access_token = authResponse.access_token;
 
             fs.writeFileSync(
               appDataGlobalConfigPath,
               JSON.stringify(globalConfigData),
               {
-                flag: 'w'
+                flag: "w",
               }
-            )
+            );
 
-            this.$router.push('/account')
+            this.$router.push("/account");
           }
-        })
-    }
-  }
-}
+        });
+    },
+  },
+};
 </script>
 
 <style></style>
